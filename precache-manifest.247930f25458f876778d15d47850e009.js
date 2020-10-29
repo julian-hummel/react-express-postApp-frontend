d6bb1e8a7d668381a85a54e5d060ebce23614237
st162721 <julian.hummel7@gmail.com>
2020-10-29 14:36:16 +0100

    Updates

---
+++ precache-manifest.247930f25458f876778d15d47850e009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3350936c4c9ac52d7280a5ba1c2c66b4",
+    "url": "/react-express-postApp-frontend/index.html"
+  },
+  {
+    "revision": "9db551b8ab07804de8fe",
+    "url": "/react-express-postApp-frontend/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6ded1792fba3b180f8e9",
+    "url": "/react-express-postApp-frontend/static/css/main.7781475d.chunk.css"
+  },
+  {
+    "revision": "9db551b8ab07804de8fe",
+    "url": "/react-express-postApp-frontend/static/js/2.8d67d3be.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react-express-postApp-frontend/static/js/2.8d67d3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ded1792fba3b180f8e9",
+    "url": "/react-express-postApp-frontend/static/js/main.05d82de0.chunk.js"
+  },
+  {
+    "revision": "e82e0fc34039d2c5aac9",
+    "url": "/react-express-postApp-frontend/static/js/runtime-main.faaa014b.js"
+  }
+]);
