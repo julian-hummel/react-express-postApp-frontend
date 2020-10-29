addd3d780dca7531929a6e3e2f295216558c6e2c
st162721 <julian.hummel7@gmail.com>
2020-10-29 16:25:57 +0100

    Updates

--- precache-manifest.0cb7ee910f1a08cc28730e76109fda18.js
+++ precache-manifest.0cb7ee910f1a08cc28730e76109fda18.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e07f1e9a57e32c597992e0795fab636f",
+    "revision": "782a2ef822e2b99c128096826c49e536",
     "url": "/react-express-postApp-frontend/index.html"
   },
   {
-    "revision": "d99361d16e1ef57416fe",
+    "revision": "bc41c25e917e8efbf3e2",
     "url": "/react-express-postApp-frontend/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "01c0cb4a9f35e3b04f77",
+    "revision": "b90ac3797d0b3426025c",
     "url": "/react-express-postApp-frontend/static/css/main.7781475d.chunk.css"
   },
   {
-    "revision": "d99361d16e1ef57416fe",
-    "url": "/react-express-postApp-frontend/static/js/2.3657a135.chunk.js"
+    "revision": "bc41c25e917e8efbf3e2",
+    "url": "/react-express-postApp-frontend/static/js/2.4c85e6c8.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/react-express-postApp-frontend/static/js/2.3657a135.chunk.js.LICENSE.txt"
+    "url": "/react-express-postApp-frontend/static/js/2.4c85e6c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "01c0cb4a9f35e3b04f77",
-    "url": "/react-express-postApp-frontend/static/js/main.364b107e.chunk.js"
+    "revision": "b90ac3797d0b3426025c",
+    "url": "/react-express-postApp-frontend/static/js/main.490e6d10.chunk.js"
   },
   {
     "revision": "e82e0fc34039d2c5aac9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-express-postApp-frontend/precache-manifest.96cceafb21dce7ba030e2ed4e4039ab5.js"
+  "/react-express-postApp-frontend/precache-manifest.0cb7ee910f1a08cc28730e76109fda18.js"
 );
 
 self.addEventListener('message', (event) => {
