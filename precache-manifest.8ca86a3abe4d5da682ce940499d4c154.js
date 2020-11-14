2addc9168d25a9fbb83404c7a19b0070e8108926
st162721 <julian.hummel7@gmail.com>
2020-11-14 18:57:34 +0100

    Updates

--- precache-manifest.8ca86a3abe4d5da682ce940499d4c154.js
+++ precache-manifest.8ca86a3abe4d5da682ce940499d4c154.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "795953505b43308ee261597cfe7c62e9",
+    "revision": "a0c38190b832f8cfaccec67c921f95ac",
     "url": "/react-express-postApp-frontend/index.html"
   },
   {
-    "revision": "10e16aa33a7c17f3972f",
+    "revision": "8183e9dfb61d279d0751",
     "url": "/react-express-postApp-frontend/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "6c3671998630a644f55e",
-    "url": "/react-express-postApp-frontend/static/css/main.b17cdef0.chunk.css"
+    "revision": "8434e5da8d4ecd249606",
+    "url": "/react-express-postApp-frontend/static/css/main.d94f8006.chunk.css"
   },
   {
-    "revision": "10e16aa33a7c17f3972f",
-    "url": "/react-express-postApp-frontend/static/js/2.e51b5888.chunk.js"
+    "revision": "8183e9dfb61d279d0751",
+    "url": "/react-express-postApp-frontend/static/js/2.6a53345f.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/react-express-postApp-frontend/static/js/2.e51b5888.chunk.js.LICENSE.txt"
+    "url": "/react-express-postApp-frontend/static/js/2.6a53345f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6c3671998630a644f55e",
-    "url": "/react-express-postApp-frontend/static/js/main.12c940ce.chunk.js"
+    "revision": "8434e5da8d4ecd249606",
+    "url": "/react-express-postApp-frontend/static/js/main.6361d29a.chunk.js"
   },
   {
     "revision": "e82e0fc34039d2c5aac9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-express-postApp-frontend/precache-manifest.af2a47649846937721dc5f02189972fb.js"
+  "/react-express-postApp-frontend/precache-manifest.8ca86a3abe4d5da682ce940499d4c154.js"
 );
 
 self.addEventListener('message', (event) => {
